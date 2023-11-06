3ba06594b480056db32450717d65ec286937062e
Alejandro Vázquez Carrión <123106711+ElTinii@users.noreply.github.com>
2023-11-06 16:27:20 +0100

    papush

--- Controlador/controlador.js
+++ Controlador/controlador.js
@@ -60,12 +60,31 @@ function init() {
     setInterval(clock, 1000);
 }
 
-(function (){
-    calculateHourDegrees();
-    calculateMinuteDegrees();
-    calculateSeconds();
+(function () {
+    setInterval(() => {
+      calculateHourDegrees();
+      calculateMinuteDegrees();
+      calculateSeconds();
+    }, 1000);
   })();
 
   function linearMap(value, min, max, newMin, newMax) {
     return newMin + ((newMax - newMin) * (value - min)) / (max - min);
+  }
+
+  function calculateHourDegrees() {
+    const currentHour = new Date().getHours() - 12;
+    const angle = linearMap(currentHour, 0, 12, 0, 360);
+    document.querySelector(".hours").style.transform = `rotate(${angle}deg)`;
+  }
+  function calculateMinuteDegrees() {
+    const currentMinutes = new Date().getMinutes();
+    const angle = linearMap(currentMinutes, 0, 60, 0, 360);
+    document.querySelector(".minutes").style.transform = `rotate(${angle}deg)`;
+  }
+  
+  function calculateSeconds() {
+    const currentMinutes = new Date().getSeconds();
+    const angle = linearMap(currentMinutes, 0, 60, 0, 360);
+    document.querySelector(".seconds").style.transform = `rotate(${angle}deg)`;
   }
