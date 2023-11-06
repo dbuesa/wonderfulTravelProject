9a31ded8b62d95e65ba6d9137a4c8a599473f566
David Buesa <113169265+dbuesa@users.noreply.github.com>
2023-11-06 21:56:57 +0100

    pp

---
+++ Controlador/export.js
@@ -0,0 +1,53 @@
+const imatges = {
+    "Praga": "Imatges/praga.webp",
+    "Madrid": "Imatges/madrid.webp",
+    "Amsterdam": "Imatges/amsterdam.webp",
+    "Atenas": "Imatges/atenas.webp",
+    "Kampala": "Imatges/kampala..jpg",
+    "Abuja": "Imatges/abuja.jpg",
+    "Brazzaville": "Imatges/brazzaville.jpg",
+    "Algeria": "Imatges/algeria.jpg",
+    "Rio de Janeiro": "Imatges/riodejaneiro.webp",
+    "Buenos Aires": "Imatges/Buenos-Aires.jpg",
+    "Bogotà": "Imatges/bogota.jpg",
+    "Cusco": "Imatges/cusco.jpg",
+    "Sydney": "Imatges/sydney.jpg",
+    "Wellington": "Imatges/wellington.jpg",
+    "Wagga Wagga": "Imatges/wagga.jpg",
+    "Hobart": "Imatges/hobart.jpg",
+    "Dacca": "Imatges/dacca.jpg",
+    "Hong Kong": "Imatges/hongkong.webp",
+    "Jakarta": "Imatges/jakarta.jpg",
+    "Canton": "Imatges/canton.jpg"
+}
+
+const preus = {
+    "Praga": 199.99,
+    "Madrid": 299.99,
+    "Amsterdam": 499.99,
+    "Atenas": 449.99,
+    "Kampala": 9999.99,
+    "Abuja": 999.99,
+    "Brazzaville": 399.99,
+    "Algeria": 299.99,
+    "Rio de Janeiro": 2999.99,
+    "Buenos Aires": 1999.99,
+    "Bogotà": 1499.99,
+    "Cusco": 2499.99,
+    "Sydney": 1499.99,
+    "Wellington": 1400.99,
+    "Wagga Wagga": 3499.99,
+    "Hobart": 1999.99,
+    "Dacca": 2199.99,
+    "Hong Kong": 3999.99,
+    "Jakarta": 2499.99,
+    "Canton": 1999.99
+}
+
+const continents = {
+    "europa": ["Praga", "Madrid", "Amsterdam", "Atenas"],
+    "africa": ['Kampala', 'Abuja', 'Brazzaville', 'Algeria'],
+    "america": ['Rio de Janeiro', 'Buenos Aires', 'Bogotà', 'Cusco'],
+    "oceania": ['Sydney', 'Wellington', 'Wagga Wagga', 'Hobart'],
+    "asia": ['Dacca', 'Hong Kong', 'Jakarta', 'Canton']
+}
