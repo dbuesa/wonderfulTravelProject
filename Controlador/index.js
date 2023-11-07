c3863649fd87d9394186f134a7550aabd2c0208a
David Buesa <113169265+dbuesa@users.noreply.github.com>
2023-11-07 14:32:05 +0100

    papush

--- Controlador/index.js
+++ Controlador/index.js
@@ -2,12 +2,13 @@ import { imatges } from "./export.js";
 import { preus } from "./export.js";
 import { continents } from "./export.js";
 
+const descompte = 0.15;
 
 document.getElementById("desti").addEventListener("change", carregarContinents);
 document.getElementById("numPersones").addEventListener("change", carregarPreu);
 document.getElementById("ciutat").addEventListener("change", carregarPreu);
 document.getElementById("ciutat").addEventListener("change", carregarImatge);
-
+document.getElementById("descompte").addEventListener("click", aplicarDescompte);
 
 function carregarContinents(){
     let continent = document.getElementById("desti").value;
@@ -15,7 +16,6 @@ function carregarContinents(){
     mostrarCiutats(ciutats);
     carregarImatge();
 }
-
 
 function mostrarCiutats(ciutats) {
     let ciutatsSelect = document.getElementById("ciutat");
@@ -38,9 +38,22 @@ function carregarPreu(){
     }
 }
 
-
 function carregarImatge(){
     let ciutat = document.getElementById("ciutat").value;
     let imatge = imatges[ciutat];
     document.getElementById("imatge").src = imatge;
+}
+
+function aplicarDescompte(){
+    let numPersones = document.getElementById("numPersones").value;
+    if(numPersones >= 3){
+        let ciutat = document.getElementById("ciutat").value;
+        let preuTotal = preus[ciutat] * numPersones;
+        let preuFinal = preuTotal - (preuTotal * descompte);
+        let pf = parseFloat(preuFinal).toFixed(2);
+        if(pf !== "NaN"){
+            document.getElementById("preu").value = pf + " €";
+            alert("Descompte aplicat: " + descompte * 100 + "% " + ". Preu final: " + pf + " €. T'has estalviat: " + (preuTotal - preuFinal).toFixed(2) + " €" );
+        }
+    }else alert("El descompte només s'aplica a partir de 3 persones");
 }
