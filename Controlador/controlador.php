a0ab7bd6e4903e2b49493000b387fa5b15f3d697
David Buesa <113169265+dbuesa@users.noreply.github.com>
2023-11-20 15:39:28 +0100

    afegir reserva i usuari papuuush

--- Controlador/controlador.php
+++ Controlador/controlador.php
@@ -1,5 +1,22 @@
 <?php
+require "../Model/model.php";
+require "../Utils/utils.php";
 
-echo "hola";
+$nom = netejarData($_POST['nom']);
+$adreca = netejarData($_POST['direccio']);
+$sexe = netejarData($_POST['sexe']);
+$adreca_electronica = netejarData($_POST['correu']);
+$dni = netejarData($_POST['dni']);
+$codi_postal = netejarData($_POST['cp']);
+$continent = netejarData($_POST['desti']);
+$ciutat = netejarData($_POST['ciutat']);
+$data = netejarData($_POST['data']);
+$numPersones = netejarData($_POST['numPersones']);
+$preu = netejarData($_POST['preu']);
 
-?>
+
+
+afegirUsuari($nom, $adreca, $sexe, $adreca_electronica, $dni, $codi_postal);
+afegirReserva($nom, $continent, $ciutat, $data, $numPersones, $preu);
+
+?>      
--- Model/model.php
+++ Model/model.php
@@ -1,23 +1,15 @@
 <?php
-
-function afegirUsuari($nom, $telefon, $adreca, $sexe, $adreca_electronica, $dni, $codi_postal){
-    require "../Database/connexio.php";
-
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-       
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      
-        $stmt = $conn->prepare("INSERT INTO usuaris (nom, telefon, adreca, sexe, adreca_electronica, dni, codi_postal) VALUES (?, ?, ?, ? , ? , ? ,?)");
+function afegirUsuari($nom, $adreca, $sexe, $adreca_electronica, $dni, $codi_postal){
+  require "../Database/connexio.php";
+    try {      
+        $stmt = $conn->prepare("INSERT INTO usuaris (nom, adreca, sexe, adreca_electronica, dni, codi_postal) VALUES (?, ?, ?, ? , ? , ?)");
     
-        $stmt->bindParam(1, $dni);
-        $stmt->bindParam(2, $nom);
-        $stmt->bindParam(3, $adreca);
-        $stmt->bindParam(4, $sexe);
-        $stmt->bindParam(5, $adreca_electronica);
-        $stmt->bindParam(6, $dni);
-        $stmt->bindParam(7, $codi_postal);
-    
+        $stmt->bindParam(1, $nom);
+        $stmt->bindParam(2, $adreca);
+        $stmt->bindParam(3, $sexe);
+        $stmt->bindParam(4, $adreca_electronica);
+        $stmt->bindParam(5, $dni);
+        $stmt->bindParam(6, $codi_postal);
         $stmt->execute();
         
       } catch(PDOException $e) {
@@ -28,6 +20,64 @@ function afegirUsuari($nom, $telefon, $adreca, $sexe, $adreca_electronica, $dni,
 
 }
 
+function obtenirIdUsuari($nom){
+  require "../Database/connexio.php";
+  try{
+    $stmt = $conn->prepare("SELECT usuari_id FROM usuaris WHERE nom = ?");
+    $stmt->bindParam(1, $nom);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result['usuari_id'];
+  } catch(PDOException $e) {
+    echo  "Error: " . $e->getMessage();
+}
+}
 
+function obtenirIdContinent($continent){
+  require "../Database/connexio.php";
+  try{
+    $stmt = $conn->prepare("SELECT continent_id FROM contintents WHERE nom = ?");
+    $stmt->bindParam(1, $continent);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result['continent_id'];
+  } catch(PDOException $e) {
+    echo  "Error: " . $e->getMessage();
+  }
+}
+
+function obtenirIdCiutat($ciutat){
+  require "../Database/connexio.php";
+  try{
+    $stmt = $conn->prepare("SELECT ciutat_id FROM ciutats WHERE nom = ?");
+    $stmt->bindParam(1, $ciutat);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result['ciutat_id'];
+  } catch(PDOException $e) {
+    echo  "Error: " . $e->getMessage();
+  }
+}
+
+
+function afegirReserva($nom, $continent, $ciutat, $data, $numPersones, $preu){
+  require "../Database/connexio.php";
+  $usuari_id = obtenirIdUsuari($nom);
+  $continent_id = obtenirIdContinent($continent);
+  $ciutat_id = obtenirIdCiutat($ciutat);
+  try{
+    $stmt = $conn->prepare("INSERT INTO reserves (usuari_id, continent_id, ciutat_id, data, persones, preu) VALUES (?, ?, ?, ? , ? , ?)");
+    $stmt->bindParam(1, $usuari_id);
+    $stmt->bindParam(2, $continent_id);
+    $stmt->bindParam(3, $ciutat_id);
+    $stmt->bindParam(4, $data);
+    $stmt->bindParam(5, $numPersones);
+    $stmt->bindParam(6, $preu);
+    $stmt->execute();
+  } catch(PDOException $e) {
+    echo  "Error: " . $e->getMessage();
+  }
+
+}
 
 ?>
