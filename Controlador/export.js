6d4809c1984e013390f147cce6121da3b33a6035
Alejandro Vázquez Carrión <123106711+ElTinii@users.noreply.github.com>
2023-11-20 15:41:00 +0100

    contintintent

---
+++ Controlador/export.js
@@ -0,0 +1,53 @@
+export const imatges = {
+    "Praga": "Imatges/praga.webp",
+    "Madrid": "Imatges/madrid.webp",
+    "Amsterdam": "Imatges/amsterdam.webp",
+    "Atenas": "Imatges/atenas.webp",
+    "Kampala": "Imatges/kampala.webp",
+    "Abuja": "Imatges/abuja.webp",
+    "Brazzaville": "Imatges/brazzaville.webp",
+    "Algeria": "Imatges/argelia.webp",
+    "Rio de Janeiro": "Imatges/riodejaneiro.webp",
+    "Buenos Aires": "Imatges/Buenos-Aires.webp",
+    "Bogotà": "Imatges/bogota.webp",
+    "Cusco": "Imatges/cusco.webp",
+    "Sydney": "Imatges/sydney.webp",
+    "Wellington": "Imatges/wellington.webp",
+    "Wagga Wagga": "Imatges/WaggaWagga.webp",
+    "Hobart": "Imatges/hobart.webp",
+    "Dacca": "Imatges/daca.webp",
+    "Hong Kong": "Imatges/hongkong.webp",
+    "Jakarta": "Imatges/jakarta.webp",
+    "Canton": "Imatges/canton.webp"
+}
+
+export const preus = {
+    "Praga": 199.99,
+    "Madrid": 299.99,
+    "Amsterdam": 499.99,
+    "Atenas": 449.99,
+    "Kampala": 9999.99,
+    "Abuja": 999.99,
+    "Brazzaville": 399.99,
+    "Algeria": 299.99,
+    "Rio de Janeiro": 2999.99,
+    "Buenos Aires": 1999.99,
+    "Bogotà": 1499.99,
+    "Cusco": 2499.99,
+    "Sydney": 1499.99,
+    "Wellington": 1400.99,
+    "Wagga Wagga": 3499.99,
+    "Hobart": 1999.99,
+    "Dacca": 2199.99,
+    "Hong Kong": 3999.99,
+    "Jakarta": 2499.99,
+    "Canton": 1999.99
+}
+
+export const continents = {
+    "europa": ["Praga", "Madrid", "Amsterdam", "Atenas"],
+    "africa": ['Kampala', 'Abuja', 'Brazzaville', 'Algeria'],
+    "america": ['Rio de Janeiro', 'Buenos Aires', 'Bogotà', 'Cusco'],
+    "oceania": ['Sydney', 'Wellington', 'Wagga Wagga', 'Hobart'],
+    "asia": ['Dacca', 'Hong Kong', 'Jakarta', 'Canton']
+}
