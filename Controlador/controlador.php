f5507e519cb7e4f0190b680ac7afefa48bd10d50
David Buesa <113169265+dbuesa@users.noreply.github.com>
2023-11-23 16:05:58 +0100

    ss

--- Controlador/controlador.php
+++ Controlador/controlador.php
@@ -5,29 +5,32 @@ require "../Model/model.php";
 require "../Utils/utils.php";
 require "../Model/enviarMail.php";
 
-$nom = netejarData($_POST['nom']);
-$adreca = netejarData($_POST['direccio']);
-$sexe = netejarData($_POST['sexe']);
-$adreca_electronica = netejarData($_POST['correu']);
-$dni = netejarData($_POST['dni']);
-$codi_postal = netejarData($_POST['cp']);
-$continent = netejarData($_POST['desti']);
-$ciutat = netejarData($_POST['ciutat']);
-$data = netejarData($_POST['data']);
-$numPersones = netejarData($_POST['numPersones']);
-$preu = netejarData($_POST['preu']);
 
-$_COOKIE['nom'] = $nom;
-$_COOKIE['adreca'] = $adreca;
-$_COOKIE['sexe'] = $sexe;
-$_COOKIE['adreca_electronica'] = $adreca_electronica;
-$_COOKIE['dni'] = $dni;
-$_COOKIE['codi_postal'] = $codi_postal;
-$_COOKIE['continent'] = $continent;
-$_COOKIE['ciutat'] = $ciutat;
-$_COOKIE['data'] = $data;
-$_COOKIE['numPersones'] = $numPersones;
-$_COOKIE['preu'] = $preu;
+if(!empty($nom) && !empty($adreca) && !empty($sexe) && !empty($adreca_electronica) && !empty($dni) && !empty($codi_postal) && !empty($continent) && !empty($ciutat) && !empty($data) && !empty($numPersones) && !empty($preu)){
+    $nom = $_POST['nom'];
+    $adreca = $_POST['direccio'];
+    $sexe = $_POST['sexe'];
+    $adreca_electronica = $_POST['correu'];
+    $dni = $_POST['dni'];
+    $codi_postal = $_POST['cp'];
+    $continent = $_POST['desti'];
+    $ciutat = $_POST['ciutat'];
+    $data = $_POST['data'];
+    $numPersones = $_POST['numPersones'];
+    $preu = $_POST['preu'];
+    $_COOKIE['nom'] = $nom;
+    $_COOKIE['adreca'] = $adreca;
+    $_COOKIE['sexe'] = $sexe;
+    $_COOKIE['adreca_electronica'] = $adreca_electronica;
+    $_COOKIE['dni'] = $dni;
+    $_COOKIE['codi_postal'] = $codi_postal;
+    $_COOKIE['continent'] = $continent;
+    $_COOKIE['ciutat'] = $ciutat;
+    $_COOKIE['data'] = $data;
+    $_COOKIE['numPersones'] = $numPersones;
+    $_COOKIE['preu'] = $preu;
+}
+
 
 
 afegirUsuari($nom, $adreca, $sexe, $adreca_electronica, $dni, $codi_postal);
