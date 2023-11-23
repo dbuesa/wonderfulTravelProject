f9460f63fc06ae7a967d9e4758aa6748cb7ecf09
David Buesa <113169265+dbuesa@users.noreply.github.com>
2023-11-23 16:02:31 +0100

    papush

--- Controlador/controlador.php
+++ Controlador/controlador.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 require "../Model/model.php";
 require "../Utils/utils.php";
 require "../Model/enviarMail.php";
@@ -15,6 +17,17 @@ $data = netejarData($_POST['data']);
 $numPersones = netejarData($_POST['numPersones']);
 $preu = netejarData($_POST['preu']);
 
+$_COOKIE['nom'] = $nom;
+$_COOKIE['adreca'] = $adreca;
+$_COOKIE['sexe'] = $sexe;
+$_COOKIE['adreca_electronica'] = $adreca_electronica;
+$_COOKIE['dni'] = $dni;
+$_COOKIE['codi_postal'] = $codi_postal;
+$_COOKIE['continent'] = $continent;
+$_COOKIE['ciutat'] = $ciutat;
+$_COOKIE['data'] = $data;
+$_COOKIE['numPersones'] = $numPersones;
+$_COOKIE['preu'] = $preu;
 
 
 afegirUsuari($nom, $adreca, $sexe, $adreca_electronica, $dni, $codi_postal);
